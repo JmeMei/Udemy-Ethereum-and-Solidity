fed7fb9338c0c6be2e36c454336842e85ccf5060
Jamie Esguerra <jamie002@e.ntu.edu.sg>
2026-05-21 18:29:16 +0800

    Refactor Car tests to initialize car instance in beforeEach for consistency

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -41,14 +41,17 @@ class Car {
     }
 }
 
+let car; 
+beforeEach(() => {
+    car = new Car();
+});
+
 describe('Car', () => {
     it('can park', () => {
-        const car = new Car();
         assert.equal(car.park(), 'stopped');
     });
 
     it('can drive', () => {
-        const car = new Car();
         assert.equal(car.drive(), 'vroom');
     });
 });
