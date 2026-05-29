29f44871d05c52c1cf45ed9234d72fbf95414cff
Jamie Esguerra <jamie002@e.ntu.edu.sg>
2026-05-29 15:53:45 +0800

    Add Inbox module: Implement initial contract setup for Inbox with default message

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config(); // Make sure this is here to read your .env file
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_URL, // This matches the key in your .env
+      accounts: [process.env.SEPOLIA_PRIVATE_KEY] // This matches the key in your .env
+    }
+  }
 };
